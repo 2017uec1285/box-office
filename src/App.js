f8f5046770c393ca8a3308ceeeae11be8236b071
2017uec1285 <2017uec1285@mnit.ac.in>
2021-01-08 00:05:23 +0530

    added Navbar and created pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 function App() {
-  return <div>hii</div>;
+  return (
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <h1 style={{ color: 'red', textAlign: 'center' }}>
+            This is 404 page.
+          </h1>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
