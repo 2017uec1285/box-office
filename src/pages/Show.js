3a8b96d1535975e8559fa2b6ee20d29b9dbaee9e
2017uec1285 <2017uec1285@mnit.ac.in>
2021-02-03 15:08:50 +0530

    useReducer added

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,12 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+const reducer=(prevState,action)=>{
+    switch(action.type){
+        case 'FETCH_SUCCESS':{
+            return { show:action.show, isLoading:false, error:false };
+        }
+        case 'FETCH_FAILED':{
+            return { ...prevState, isLoading: false, error: action.error };
+        }
+        default: return prevState;
+    }
+}
+
+const initialState={
+    show:null,
+    isLoading:true,
+    error:null
+}
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [{ isLoading,error,show},dispatch]=useReducer(reducer,initialState);
+
+//   const [show, setShow] = useState(null);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
 
   console.log(show);
   let isMounted = true;
@@ -14,14 +33,16 @@ const Show = () => {
     apiGet(`/shows/${id}?embed[]=episodes&embed[]=cast`)
       .then(results => {
         if (isMounted) {
-          setShow(results);
-          setIsLoading(false);
+            dispatch( { type: 'FETCH_SUCCESS',show:results } );
+        //   setShow(results);
+        //   setIsLoading(false);
         }
       })
       .catch(err => {
         if (isMounted) {
-          setIsLoading(false);
-          setError(`An error occured ${err.message}`);
+            dispatch( { type: 'FETCH_FAILED',show:err.message } );
+        //   setIsLoading(false);
+        //   setError(`An error occured ${err.message}`);
         }
       });
     return () => {
@@ -34,6 +55,7 @@ const Show = () => {
   if (error) {
     return <div>{error}</div>;
   }
+  
   return <div>{id}</div>;
 };
 export default Show;
