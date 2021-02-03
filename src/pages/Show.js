e80060ac88ca566ffb6f093f8a754c6d3bd4d9c9
2017uec1285 <2017uec1285@mnit.ac.in>
2021-02-03 13:22:35 +0530

    modified show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,12 +5,35 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   console.log(show);
+  let isMounted = true;
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=episodes&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    apiGet(`/shows/${id}?embed[]=episodes&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setIsLoading(false);
+          setError(`An error occured ${err.message}`);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
   return <div>{id}</div>;
 };
 export default Show;
